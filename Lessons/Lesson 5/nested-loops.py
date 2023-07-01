64bcb2565c8016ab36f5653a99262a4411194748
VarunHari12 <varunhari@outlook.com>
2023-07-01 14:38:51 -0700

    Finished loops

--- Lessons/Lesson 5/nested-loops.py
+++ Lessons/Lesson 5/nested-loops.py
@@ -6,4 +6,23 @@
 for column in range(1,4):
     for row in range(1,5):
         print("0",end = "")
-    
+    print("\n",end = "")
+
+# so how does this run?
+# the second loop actually runs a total of 12 times
+# essentially it prints 4 consectuive 0's
+# next is starts a new line
+# it repeats this process 3 times
+
+
+i = 1
+while i <= 4 :
+    j = 0
+    while  j <= 3 :
+        print(i*j, end=" ")
+        j += 1
+    print()
+    i += 1
+
+# this prints out the first 3 mutliples of the numbers 1-4
+# lets walk throught this
